66ac5dbccc4f844706e4b7262c331d7ce8c90331
Aanchal Agarwal <aanchal.agarwal@nashtechglobal.com>
2023-12-20 12:18:23 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from './Header.svelte';
+</script>
+
+<main>
+  <Header title="Responsive Header" />
+  <!-- Your content goes here -->
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1rem;
+  }
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let title;
+</script>
+
+<header>
+  <h1>{title}</h1>
+</header>
+
+<style>
+  header {
+    background-color: #333;
+    color: #fff;
+    padding: 1rem;
+    text-align: center;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  @media (max-width: 600px) {
+    header {
+      padding: 0.5rem;
+    }
+  }
+</style>
